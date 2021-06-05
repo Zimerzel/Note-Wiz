c1fe6752c14a5799b45c12ba922a12a6b01bd193
Imerzel <zach.imerzel@gmail.com>
2021-06-05 15:20:58 -0500

    heroku port

--- Develop/server.js
+++ Develop/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 var app = express();
-const port = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
